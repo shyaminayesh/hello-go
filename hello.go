54caa6be65fb1c1c4b12023fee0f4373614b69be
Shyamin Ayesh <me@shyamin.com>
2022-10-16 15:55:47 +0530

    add some login for debug

--- hello.go
+++ hello.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+
+	log.Println("Starting the hello-go")
 
 	/**
 	* Handle the / route. It'll return hello world as the
 	* http body.
 	 */
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Request received on / route")
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		fmt.Fprintf(w, "<center><pre>hello world ... !</pre></center>")
 	})
@@ -20,6 +24,7 @@ func main() {
 	* Start the http server and listen on the port
 	* 8080 for the incoming request to the application.
 	 */
+	log.Println("Serving content on :8080")
 	http.ListenAndServe("localhost:8080", nil)
 
 }
